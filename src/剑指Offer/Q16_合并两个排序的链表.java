7190ad332cda69519a073d6e7aadd548f70d5523
loveincode <huonefl@163.com>
2017-12-15 20:05:00 +0800

    up

--- src/剑指Offer/Q16_合并两个排序的链表.java
+++ src/剑指Offer/Q16_合并两个排序的链表.java
@@ -1,5 +1,9 @@
 package 剑指Offer;
 
+import javax.xml.ws.AsyncHandler;
+
+import common.ListNode;
+
 /**
  *
  * @author loveincode
@@ -15,7 +19,20 @@ public class Q16_合并两个排序的链表 {
 	 */
 
 	public ListNode Merge(ListNode list1, ListNode list2) {
-
+		if(list1==null) {
+			return list2;
+		}
+		if(list2==null) {
+			return list1;
+		}
+		ListNode node = null;
+		if(list1.val<=list2.val) {
+			node = list1;
+			node.next = Merge(list1.next, list2);
+		}else {
+			node = list2;
+			node.next = Merge(list1, list2.next);
+		}
+		return node;
 	}
-
 }
