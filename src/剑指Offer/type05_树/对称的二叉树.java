fcafe6b2b9c961e2aa5aa29815bacf33bb83bd0b
loveincode <huonefl@163.com>
2018-01-30 20:11:06 +0800

    upupup

--- src/剑指Offer/type05_树/对称的二叉树.java
+++ src/剑指Offer/type05_树/对称的二叉树.java
@@ -1,5 +1,24 @@
 package 剑指Offer.type05_树;
 
-public class 对称的二叉树 {
+import common.TreeNode;
 
+public class 对称的二叉树 {
+	
+	/*
+	 * 请实现一个函数，用来判断一颗二叉树是不是对称的。
+	 * 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+	 */
+	
+	boolean isSymmetrical(TreeNode pRoot)
+    {	
+		isSymmetrical(pRoot.left);
+        isSymmetrical(pRoot.right);
+        if (pRoot==null) 
+        if(pRoot.left!=null && pRoot.right!=null && pRoot.left==pRoot.right) {
+        }
+        else {
+        	return false;
+        }
+        
+    }
 }
